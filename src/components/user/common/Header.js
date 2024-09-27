5cf5ef3c24b20e0978d8cc67ae1baa1f93ae0fd3
Mik <mikchudhari96@gmail.com>
2024-09-27 12:44:31 -0700

    Home and Footer page add

--- src/components/user/common/Header.js
+++ src/components/user/common/Header.js
@@ -1,35 +1,107 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isSticky, setSticky] = useState(false); 
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // Effect to add/remove class from body
+  useEffect(() => {
+    if (isMobileMenuOpen) {
+      document.body.classList.add('mobile-nav-active');
+    } else {
+      document.body.classList.remove('mobile-nav-active');
+    }
+    // Cleanup the class when the component is unmounted
+    return () => {
+      document.body.classList.remove('mobile-nav-active');
+    };
+  }, [isMobileMenuOpen]);
+
+    // Effect to track scroll position
+    useEffect(() => {
+      const handleScroll = () => {
+        // Set sticky if the user has scrolled down 100 pixels
+        if (window.scrollY > 100) {
+          setSticky(true);
+        } else {
+          setSticky(false);
+        }
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      // Cleanup the event listener when component unmounts
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/protfolio">Protfolio</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+    <header id="header" className={`header d-flex align-items-center ${isSticky ? 'sticky-top' : ''}`}>
+      <div className="container-fluid container-xl position-relative d-flex align-items-center">
+
+        <a href="index.html" className="logo d-flex align-items-center me-auto">
+          {/* <!-- Uncomment the line below if you also wish to use an image logo -->
+          <!-- <img src="assets/img/logo.png" alt=""> --> */}
+          <h1 className="sitename">DexCoder</h1>
+        </a>
+
+        <nav id="navmenu" className="navmenu">
+          <ul classNameName="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </li>
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" to="/services">
+                  Services
+                </Link>
+              </li>
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" to="/protfolio">
+                  Portfolio
+                </Link>
+              </li>
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li classNameName="nav-item">
+                <Link classNameName="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+            </ul>
+            <i
+            className={`mobile-nav-toggle d-xl-none bi ${isMobileMenuOpen ? 'bi-x' : 'bi-list'}`}
+            onClick={toggleMobileMenu}
+          ></i>
+        </nav>
+        <a className="btn-getstarted" href="index.html#about">Get Started</a>
+
+      </div>
     </header>
+    </>
   );
 };
 
--- src/components/user/pages/Home.js
+++ src/components/user/pages/Home.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Header from '../common/Header';
+import Footer from '../common/Footer';
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-      <h1>Home Page</h1>
-      <p>Welcome to the home page.</p>
-    </div>
+    <>
+    <Header />
+    <section id="hero" className="hero section">
+      <div className="container">
+        <div className="row gy-4">
+          <div className="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+            <h1>Grow your business with Vesperr</h1>
+            <p>We are a team of talented designers making websites with Bootstrap</p>
+            <div className="d-flex">
+              <a href="#about" className="btn-get-started">Get Started</a>
+              
+            </div>
+          </div>
+          <div className="col-lg-6 order-1 order-lg-2 hero-img">
+            <img 
+              src="assets/img/hero-img.png" 
+              className="img-fluid animated" 
+              alt="Hero" 
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <Footer />
+    </>
   );
 };
 
